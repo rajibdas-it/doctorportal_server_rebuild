9c5c9262708dcc4a7091a00f3dbabb8844838d8d
rajibdas-it <rajibrad@gmail.com>
2023-10-08 01:58:44 +0600

    create global error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,18 +1,42 @@
+/* eslint-disable no-undefined */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable prefer-const */
 import { ErrorRequestHandler } from 'express'
-import httpStatus from 'http-status'
+import { IGenericErrorMessage } from '../../interfaces/error'
+import ApiError from '../../errors/apiErrors'
+import { config } from '../../config'
+import { errorLogger } from '../shared/logger'
 
-const globalErrorHandler: ErrorRequestHandler = async (error, req, res) => {
-  res.status(httpStatus.NOT_FOUND).json({
-    status: httpStatus.NOT_FOUND,
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  config.node_env === 'development'
+    ? console.log('Global Error Handler:', error)
+    : errorLogger.error('Global Error Handler:', error)
+
+  let statusCode = 500
+  let message = 'something went wrong'
+  let errorMessages: IGenericErrorMessage[] = []
+
+  if (error instanceof Error) {
+    message = error?.message
+    errorMessages = error?.message
+      ? [{ path: '', message: error?.message }]
+      : []
+  } else if (error instanceof ApiError) {
+    statusCode = error?.statusCode
+    message = error?.message
+    errorMessages = error?.message
+      ? [{ path: '', message: error?.message }]
+      : []
+  }
+
+  res.status(statusCode).json({
     success: false,
-    message: 'Something went wrong!',
-    error: [
-      {
-        path: '',
-        message: error.message,
-      },
-    ],
+    message,
+    errorMessages,
+    stack: config.node_env != 'production' ? error?.stack : undefined,
   })
+
+  next()
 }
 
 export default globalErrorHandler
--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,6 @@ import path from 'path'
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export const config = {
+  node_env: process.env.NODE_ENV,
   port: process.env.PORT,
 }
